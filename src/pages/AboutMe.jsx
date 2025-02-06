96bda641addab5d3d66a7519df7ff1d707d180cc
pechikonar <muthukonaruot@gmail.com>
2025-02-06 09:40:54 -0500

    updated the AboutMe pagfe with proper formating and details

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -3,12 +3,18 @@ import './portfolio.css';
 
 const AboutMe = () => {
   return (
-    <section id="about">
-      <h2>About Me</h2>
-      <img src="./src/pages/me.jpg" alt="Your Avatar"  />
+    <section id="about" className="container my-5">
+      <h2 className="text-center mb-4">About Me</h2>
+      
+      <div className="project-card">
+      <img src="./src/pages/weather.jpg" alt="Your Avatar" className="img-fluid mb-3" />
       <p>
-    Hi! I am a full stack developer 
+    Hi! Hope you all doing well!.......
+    I am a full stack developer, learning front end and backend development skills from UoFT. 
+    Please check the Portfolio page to view my projects submitted so far and contact me using the contact page for any questions. 
+    You can also download my resume from Resume page.
       </p>
+      </div>
     </section>
   );
 }
