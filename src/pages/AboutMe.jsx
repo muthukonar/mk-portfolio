c4c40ffe9f1878a04a7dc53b55ebe50fd9eca595
pechikonar <muthukonaruot@gmail.com>
2025-02-06 18:31:01 -0500

    Code updated in Portfolio.jsx and AboutMe.jsx to handle the images as needed for Netlify deployments

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '/src/css/portfolio.css';
 
+import WeatherImg from '../assets/pics/weather.jpg';
+
 const AboutMe = () => {
   return (
     <section id="about" className="container my-5 bg-secondary">
       <h2 className="text-left mb-4 bold-underline">About Me</h2>
       <div className="project1-card">
-        <img src="./src/assets/pics/weather.jpg" alt="Your Avatar" className="img-fluid mb-3" />
+        <img src={WeatherImg} alt="Your Avatar" className="img-fluid mb-3" />
         </div>
         <p>
           Hi! Hope you all doing well!.......
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,46 +1,50 @@
 import React from 'react';
 import '/src/css/portfolio.css';
-
+import WeatherDashboardImg from '../assets/pics/weather.jpg'
+import VehicleBuilderImg from '../assets/pics/vehicle.jpg'
+import BUMSYSTImg from '../assets/pics/BUMSYST.jpg'
+import EmployeeTrackerImg from '../assets/pics/employeetracker.jpg'
+import ReadMeGeneratorImg from '../assets/pics/READMEMAKER.jpg'
+import PreWorkImg from '../assets/pics/prework.jpg'
 const Portfolio = () => {
   const projects = [
     {
       title: 'Weather Dashboard',
-      image: './src/assets/pics/weather.jpg',
+      image: WeatherDashboardImg,
       deployedLink: 'https://weatherdashboard-1chl.onrender.com',
       githubLink: 'https://github.com/muthukonar/WeatherDashboard',
     },
     {
       title: 'Vehicle Builder',
-      image: './src/assets/pics/vehicle.jpg',
+      image: VehicleBuilderImg,
       deployedLink: 'https://drive.google.com/file/d/1N0XzP8dQ1f7hKvBiSiNQ5enkrLzLNJYp/view?usp=drive_link',
       githubLink: 'https://github.com/muthukonar/vehiclebuilder',
     },
     {
       title: 'BUMSYST - Budget Management System',
-      image: './src/assets/pics/BUMSYST.jpg',
+      image: BUMSYSTImg,
       deployedLink: 'https://muthukonar.github.io/group4-project1',
       githubLink: 'https://github.com/muthukonar/group4-project1',
     },
     {
       title: 'Employee Tracker',
-      image: './src/assets/pics/employeetracker.jpg',
+      image: EmployeeTrackerImg,
       deployedLink: 'https://drive.google.com/file/d/1JxPJKHiIcJ68ANqc7sPW2lWxgMryJfOj/view?usp=drive_link',
       githubLink: 'https://github.com/muthukonar/EmployeeTracker',
     },
     {
       title: 'README Generator',
-      image: './src/assets/pics/READMEMAKER.jpg',
+      image: ReadMeGeneratorImg,
       deployedLink: 'https://drive.google.com/file/d/1uGMPkxLf4PXKVbEpbDOPmjLUWMXBdnZV/view?usp=sharing',
       githubLink: 'https://github.com/muthukonar/readmemaker',
     },
     {
       title: 'Prework Study Guide',
-      image: './src/assets/pics/prework.jpg',
+      image: PreWorkImg,
       deployedLink: 'https://muthukonar.github.io/prework-study-guide/',
       githubLink: 'https://github.com/muthukonar/prework-study-guide',
     },
   ];
-
   return (
     <section id="portfolio" className="container my-5">
       <h2 className="text-left mb-4 bold-underline">Portfolio</h2>
@@ -63,5 +67,4 @@ const Portfolio = () => {
     </section>
   );
 }
-
 export default Portfolio;
