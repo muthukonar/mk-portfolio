5d0d5e4b511726831cc5c6acb2969fbfcc9e2797
pechikonar <muthukonaruot@gmail.com>
2025-02-07 10:30:46 -0500

    GHITGUB and LINKEDIN logos added to footer code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
+import linkedinLogo from '../assets/pics/LI-In-Bug.png';
+import githubLogo from '../assets/pics/GitHub_Logo_White.png';
+
 const Footer = () => {
   return (
     <footer className="bg-dark text-white text-center py-3">
       <p>&copy; Feb 2025 My Portfolio. All rights reserved.</p>
       <p>
-        <a href="https://github.com/muthukonar" className="text-white" target="_blank" rel="noopener noreferrer">GitHub</a> | 
-        <a href="https://linkedin.com/in/muthu-konar-052340112" className="text-white" target="_blank" rel="noopener noreferrer"> LinkedIn</a>
+        <a href="https://github.com/muthukonar" className="text-white" target="_blank" rel="noopener noreferrer">
+          <img src={githubLogo} alt="GitHub" style={{ width: '24px', marginRight: '8px' }} />
+          
+        </a> 
+                <a href="https://linkedin.com/in/muthu-konar-052340112" className="text-white" target="_blank" rel="noopener noreferrer">
+          <img src={linkedinLogo} alt="LinkedIn" style={{ width: '24px', marginRight: '8px' }} />
+         
+        </a>
       </p>
     </footer>
   );
